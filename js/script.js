bc7e3bb42f8570a3afb1d2f3cc7874b7285eef0d
Aashish Chapagai <chapagaia7@gmail.com>
2023-08-28 12:15:35 +0545

    Validations

--- js/script.js
+++ js/script.js
@@ -1,28 +1,83 @@
-let resultdisplay = '';
+let resultdisplay = ''
 let result = document.getElementById('resultdisplay')
-let buttons = document.querySelectorAll('.buttons');
+let buttons = document.querySelectorAll('.buttons')
 
 buttons.forEach(btn => btn.addEventListener('click', function(button) {
     display(button.target.innerText)
 }))
 
 const display = (buttonvalue) => {
-    if (resultdisplay.charAt(0) == '0') {
+    if (resultdisplay.charAt(0) == '0' && resultdisplay.charAt(1) != '.') {
         resultdisplay = resultdisplay.substring(1)
     } else {
-        if (buttonvalue == 'DEL') {
-            resultdisplay = resultdisplay.slice(0, -1)
-        } else if (buttonvalue == 'RESET') {
-            resultdisplay = ''
-        } else if (buttonvalue == '=') {
-            calculation()
-        } else {
-            resultdisplay += buttonvalue
+        if (buttonvalue == '=') {
+            if (resultdisplay.charAt(resultdisplay.length - 1) == '*' || resultdisplay.charAt(resultdisplay.length - 1) ==  '/' || resultdisplay.charAt(resultdisplay.length - 1) ==  '+' || resultdisplay.charAt(resultdisplay.length - 1) == '-' || resultdisplay.charAt(resultdisplay.length - 1) == '.') {
+
+            } else {
+                result.innerText = eval(resultdisplay)
+                resultdisplay = String(eval(resultdisplay))
+            }
         }
-        if (resultdisplay == '') {
-            result.innerText = '0'
-        } else {
-            result.innerText = resultdisplay
+        else {
+            if (buttonvalue == 'DEL') {
+                resultdisplay = resultdisplay.slice(0, -1)
+            } else if (buttonvalue == 'X') {
+                if (resultdisplay == '0' || resultdisplay == '') {
+
+                } else if (resultdisplay.charAt(resultdisplay.length - 1) == '*' || resultdisplay.charAt(resultdisplay.length - 1) ==  '/' || resultdisplay.charAt(resultdisplay.length - 1) ==  '+' || resultdisplay.charAt(resultdisplay.length - 1) == '-' || resultdisplay.charAt(resultdisplay.length - 1) == '.') {
+
+                }
+                else {
+                    resultdisplay += '*'
+                }
+            } else if (buttonvalue == '/') {
+                if (resultdisplay == '0' || resultdisplay == '') {
+
+                } else if (resultdisplay.charAt(resultdisplay.length - 1) == '*' || resultdisplay.charAt(resultdisplay.length - 1) ==  '/' || resultdisplay.charAt(resultdisplay.length - 1) ==  '+' || resultdisplay.charAt(resultdisplay.length - 1) == '-' || resultdisplay.charAt(resultdisplay.length - 1) == '.') {
+
+                }
+                else {
+                    resultdisplay += '/'
+                }
+            } else if (buttonvalue == '.') {
+                test = resultdisplay.split(/[+\-/*]/)
+                if (resultdisplay == '0' || resultdisplay == '') {
+                    resultdisplay = '0.'
+                } else if (resultdisplay.charAt(resultdisplay.length - 1) == '*' || resultdisplay.charAt(resultdisplay.length - 1) ==  '/' || resultdisplay.charAt(resultdisplay.length - 1) ==  '+' || resultdisplay.charAt(resultdisplay.length - 1) == '-' || resultdisplay.charAt(resultdisplay.length - 1) == '.') {
+
+                } else if (((test[test.length-1]).split('.')).length > 1) {
+
+                } else {
+                    resultdisplay += '.'
+                }
+            } else if (buttonvalue == '+') {
+                if (resultdisplay == '0' || resultdisplay == '') {
+
+                } else if (resultdisplay.charAt(resultdisplay.length - 1) == '*' || resultdisplay.charAt(resultdisplay.length - 1) ==  '/' || resultdisplay.charAt(resultdisplay.length - 1) ==  '+' || resultdisplay.charAt(resultdisplay.length - 1) == '-' || resultdisplay.charAt(resultdisplay.length - 1) == '.') {
+
+                }
+                else {
+                    resultdisplay += '+'
+                }
+            } else if (buttonvalue == '-') {
+                if (resultdisplay == '0' || resultdisplay == '') {
+
+                } else if (resultdisplay.charAt(resultdisplay.length - 1) == '*' || resultdisplay.charAt(resultdisplay.length - 1) ==  '/' || resultdisplay.charAt(resultdisplay.length - 1) ==  '+' || resultdisplay.charAt(resultdisplay.length - 1) == '-' || resultdisplay.charAt(resultdisplay.length - 1) == '.') {
+
+                }
+                else {
+                    resultdisplay += '-'
+                }
+            } else if (buttonvalue == 'RESET') {
+                resultdisplay = ''
+            }  else {
+                resultdisplay += buttonvalue
+            }
+            if (resultdisplay == '') {
+                result.innerText = '0'
+            } else {
+                result.innerText = resultdisplay
+            }
         }
     }
 }
